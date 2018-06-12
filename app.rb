0642861981ba514754f4ede57521875978432d9d
zoeelarson01 <zoeelarson@gmail.com>
2018-06-12 17:45:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Zoe"
+  end
+  
+  get '/hometown' do
+    "My hometown is Hollywood, Florida"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Little of Your Love by Haim"
+  end
+  
 end
